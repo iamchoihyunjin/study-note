6d2916a2ec0ad85952d0b9f27363ad1f62dbcd2f
Hyunjin Choi <hyeonisism@gmail.com>
2020-01-26 12:44:35 +0900

    Add generic array list queue code

--- src/main/java/structure/queue/GenericArrayListQueue.java
+++ src/main/java/structure/queue/GenericArrayListQueue.java
@@ -1,4 +1,28 @@
 package structure.queue;
 
-public class GenericArrayListQueue {
+import java.util.ArrayList;
+
+public class GenericArrayListQueue<T> {
+
+    ArrayList<T> queue = new ArrayList<T>();
+
+    private boolean hasElements() {
+        return !queue.isEmpty();
+    }
+
+    public T peek() {
+        if (this.hasElements())
+            return queue.get(0);
+        return null;
+    }
+
+    public boolean add(T element) {
+        return queue.add(element);
+    }
+
+    public T poll() {
+        if (this.hasElements())
+            return queue.remove(0);
+        return null;
+    }
 }
